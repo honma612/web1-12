343e9cd150e1b092e69b755aee3d4b028491d82d
honma612 <oshiri_hakase@yahoo.co.jp>
2017-06-26 17:14:55 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,1 +1,28 @@
+var num = parseInt(window.prompt('1~100の数字を入力'));
 
+while(num > 100 && num < 1)
+{
+  if(num > 100 || num < 1)
+    num = parseInt(window.prompt('1~100の数字を入力'));
+}
+
+for(var i = 1; i < num; i++)
+{
+  var li = document.createElement('li');
+  if(i / 3 != 0 && i / 5 != 0)
+  {
+    li.textContent = i;
+    document.getElementById('choice').appendChild(li);
+  }
+  else if(i / 5 == 0)
+  {
+    li.textContent = 'Fizz';
+    document.getElementById('choice').appendChild(li);
+  }
+  else if(i / 3 == 0)
+  {
+    li.textContent = 'Buzz';
+    document.getElementById('choice').appendChild(li);
+  }
+ 
+}

9e0a067b199364d918c764b40df3ca67d05fa494
honma612 <oshiri_hakase@yahoo.co.jp>
2017-06-26 17:53:28 +0900

    Update default.js

--- default.js
+++ default.js
@@ -9,25 +9,24 @@ for(var i = 0; i < num; i++)
 {
   var li = document.createElement('li');
     
-  if((i + 1) % 15 == 0)
+  if((i + 1) % 3 != 0 && (i + 1) % 5 != 0)
   {
-    li.textContent = 'FizzBuzz';
-    document.getElementById('choice').appendChild(li);       
-  }
-  else if((i + 1) % 3 == 0 && (i + 1) % 15 != 0)
+    li.textContent = i + 1;
+    document.getElementById('choice').appendChild(li);     
+  }        
+  if((i + 1) % 3 == 0 && (i + 1) % 15 != 0)
   {
     li.textContent = 'Fizz';
     document.getElementById('choice').appendChild(li);
   }  
-  else if((i + 1) % 5 == 0 && (i + 1) % 15 != 0)
+  if((i + 1) % 5 == 0 && (i + 1) % 15 != 0)
   {
     li.textContent = 'Buzz';
     document.getElementById('choice').appendChild(li);
   } 
- else
- {
-    li.textContent = i + 1;
-    document.getElementById('choice').appendChild(li);     
- }
-
+  if((i + 1) % 15 == 0)
+  {
+    li.textContent = 'FizzBuzz';
+    document.getElementById('choice').appendChild(li);       
+  }    
 }

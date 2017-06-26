5d3d2daa02bb66a8aa7e31dc55a5a8186b5683e8
honma612 <oshiri_hakase@yahoo.co.jp>
2017-06-26 17:27:43 +0900

    Update default.js

--- default.js
+++ default.js
@@ -11,7 +11,7 @@ for(var i = 0; i < num; i++)
   var li = document.createElement('li');
   if((i + 1) % 3 != 0 && (i + 1) % 5 != 0)
   {
-    li.textContent = i;
+    li.textContent = i + 1;
     document.getElementById('choice').appendChild(li);
   }
   else if((i + 1) % 5 == 0)
